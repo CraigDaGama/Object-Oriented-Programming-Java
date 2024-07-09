45d2498e3a6c42741fb16e447c5e29d3c136182c
Craig <craigdagama@gmail.com>
2024-07-09 07:47:34 +0530

    Add files via upload

---
+++ MathsProgram1.java
@@ -0,0 +1,11 @@
+public class MathsProgram1 {
+    public static void main(String args[]){
+        double x=5.5, y=2.5;
+        System.out.println("using r int("+ x +")= " + Math.rint(x));
+        System.out.println("using r int("+ y +")= " + Math.rint(y));
+        System.out.println("using round("+ x +")= " + Math.round(x));
+        System.out.println("using round("+ y +")= " + Math.round(y));
+        System.out.println("using ceil("+ x +")= " + Math.ceil(x));
+        System.out.println("using floor("+ y +")= " + Math.floor(y));
+    }
+}

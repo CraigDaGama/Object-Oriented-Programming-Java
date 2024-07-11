b6d4ae408551438915dacaca872133cf76f26a0e
Craig <craigdagama@gmail.com>
2024-07-11 23:25:54 +0530

    Add files via upload

---
+++ IntegerConstants.java
@@ -0,0 +1,8 @@
+public class IntegerConstants {
+    public static void main(String args[]){
+        int x1 = 037;
+        int x2 = 0x1F;
+        int x3 = 0b00_11;
+        int x4 = 11_123;
+    }
+}

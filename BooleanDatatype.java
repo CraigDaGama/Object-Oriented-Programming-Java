5f0b5b19c3f6a0bc336102f9f45f8efccfd35133
Craig <craigdagama@gmail.com>
2024-07-13 22:08:56 +0530

    Add files via upload

---
+++ BooleanDatatype.java
@@ -0,0 +1,8 @@
+public class BooleanDatatype {
+    public static void main(String args[]) {
+        int a = 3;
+        int b = 5;
+        boolean ans = a>b;
+        System.out.println(a+">"+b+"="+ans);
+    }
+}

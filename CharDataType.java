0d807f8a49d17cc4f39ecbc1af41130f4f0867b8
Craig <craigdagama@gmail.com>
2024-07-14 10:40:36 +0530

    Add files via upload

---
+++ CharDataType.java
@@ -0,0 +1,11 @@
+public class CharDataType {
+    public static void main(String args[]) {
+        char x = 'a';
+        System.out.println("before x++ x=" + x);
+        x++;
+        System.out.println("after x++=" + + x);
+        x--;
+        System.out.println("after x--=" + + x);
+        System.out.printf("%d",(int)x);
+    }
+}

3a59ba40e80b0ae8c5886548843a819271b90757
Craig <craigdagama@gmail.com>
2024-07-23 22:23:45 +0530

    Add files via upload

---
+++ RealConstants.java
@@ -0,0 +1,10 @@
+public class RealConstants {
+    public static void main(String args[]) {
+        double x1 = 2.3;
+        float x2 = 2.56F;
+        double x3 = 34_56.89_90;
+        System.out.println("real constant x1=" + x1);
+        System.out.println("real constant x2=" + x2);
+        System.out.println("real constant x3=" + x3);
+    }
+}
